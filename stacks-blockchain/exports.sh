8553049285b67a539ddf68666ece2a8ec7f13175
muthusleuths <muthu@burnchain.io>
2024-03-26 00:54:04 -0400

    First Commit

---
+++ stacks-blockchain/exports.sh
@@ -0,0 +1,30 @@
+export APP_STACKS_IP="10.21.21.100"
+export APP_STACKS_PORT="3998"
+export APP_STACKS_BLOCKCHAIN_IP="10.21.21.99"
+export APP_STACKS_CORE_RPC_PORT="20443"
+export APP_STACKS_CORE_P2P_PORT="20444"
+export APP_STACKS_CORE_EVENT_PORT="3700"
+export APP_STACKS_API_IP="10.21.21.98"
+export APP_STACKS_API_PORT="3999"
+export APP_STACKS_DB_IP="10.21.21.97"
+export APP_STACKS_DB_PORT="5432"
+export APP_STACKS_EXPLORER_IP="10.21.21.221"
+export APP_STACKS_EXPLORER_PORT="4010"
+export APP_STACKS_DATA_DIR="${EXPORTS_APP_DIR}/data/stacks-blockchain/mainnet"
+
+
+#!/usr/bin/env bash
+
+CONFIG="${UMBREL_ROOT}/app-data/stacks-blockchain/data/config/Config.toml"
+
+set -e
+
+     sed -i  -e "s@APP_BITCOIN_NODE_IP@$APP_BITCOIN_NODE_IP@" \
+             -e "s@APP_BITCOIN_RPC_USER@$APP_BITCOIN_RPC_USER@" \
+             -e "s@APP_BITCOIN_RPC_PASS@$APP_BITCOIN_RPC_PASS@" \
+             -e "s@APP_BITCOIN_RPC_PORT@$APP_BITCOIN_RPC_PORT@" \
+             -e "s@APP_STACKS_CORE_RPC_PORT@$APP_STACKS_CORE_RPC_PORT@" \
+             -e "s@APP_STACKS_CORE_P2P_PORT@$APP_STACKS_CORE_P2P_PORT@" \
+             -e "s@APP_STACKS_API_IP@$APP_STACKS_API_IP@" \
+             -e "s@APP_STACKS_CORE_EVENT_PORT@$APP_STACKS_CORE_EVENT_PORT@" \
+             -e "s@APP_BITCOIN_P2P_PORT@$APP_BITCOIN_P2P_PORT@" "$CONFIG"
